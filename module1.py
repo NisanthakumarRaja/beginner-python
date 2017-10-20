c6dc5678f8f158430486f9f2469c4d04585e928d
NisanthakumarRaja <32951399+NisanthakumarRaja@users.noreply.github.com>
2017-10-20 15:39:08 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece017
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece017 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=10
+    b=15
+    c=20
+    d=25
+    print(a+b)
+    print(b-c)
+    print(a/c)
+    print(a+d)
+    print(c-d)
+    print(d-a)
+    print(c/d)
+if __name__ == '__main__':
+    main()
