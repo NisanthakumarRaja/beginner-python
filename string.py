61d4bfc96f583e9cddf9ec63c8622726bde17ac6
NisanthakumarRaja <32951399+NisanthakumarRaja@users.noreply.github.com>
2017-10-21 10:01:39 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece017
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece017 2017
+# Licence:     <your licence>
+#----------------------------------------------------------------------------
+
+def main():
+    mystring='hai from raja'
+    print(mystring.split(" "))
+    print(mystring.find("r"))
+if __name__ == '__main__':
+    main()
